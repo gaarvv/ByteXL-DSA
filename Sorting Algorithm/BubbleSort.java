485afa5e638db0b0fb8f96dc385824fa3f0835fd
Garv <garvshrivastava2403@gmail.com>
2025-06-25 10:12:19 +0530

    Correct Code

--- Sorting Algorithm/BubbleSort.java
+++ Sorting Algorithm/BubbleSort.java
@@ -1,8 +1,8 @@
-public class BubbleSort{
-    public static void bubbleSort(int[] arr){
+public class bubbleSort{
+    public static void BubbleSort(int[] arr){
         for(int i=0; i<arr.length; i++){
             for(int j=0; j<arr.length-1; j++){
-                if(arr[j] < arr[j+1]){
+                if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
@@ -15,6 +15,6 @@ public class BubbleSort{
     }
     public static void main(String[] args){
         int[] arr = {5,8,3,7,5,0,7};
-        bubbleSort(arr);
+        BubbleSort(arr);
     }
 }
